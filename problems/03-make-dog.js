60e0d8fb681f0b203e7ccafea6ed6d693c836bc6
seanjeyam <rohit.seanj@gmail.com>
2024-04-30 21:58:43 -0500

    Phase 3

--- problems/03-make-dog.js
+++ problems/03-make-dog.js
@@ -1,39 +1,41 @@
 class Dog {
-	constructor(name) {
-		this.name = name
-	}
-	static makeJet(...names) {
-		const jet = new this.name
-		Object.assign(this.name)
-		return jet
-	}
+  constructor(name) {
+    this.name = name; // 1st run = Jet
+  }
+  static makeJet(name) {
+    // Makes new name instance
+    const jet = new Dog("Jet");
+    // Merges original and new object together (Modifies both objects)
+    // Object.assign(this.name);
+    //returning new instance of name
+    return jet;
+  }
 
-	changeName(newName) {
-		this.name = newName;
-	}
+  changeName(newName) {
+    this.name = newName; // assigns new value to predetermined variable
+  }
 
-	speak(word) {
-		return `${this.name} says ${word}`
-	}
+  speak(word) {
+    return `${this.name} says ${word}`;
+  }
 }
 
 let dog1 = Dog.makeJet(); // returns an object
 
-console.log(dog1.name);                 // Jet
-console.log(dog1.speak("hello"));       // Jet says hello
+console.log(dog1.name); // Jet
+console.log(dog1.speak("hello")); // Jet says hello
 console.log(dog1.changeName("Freyja")); // Freyja
-console.log(dog1.name);                 // Freyja
-console.log(dog1.speak("hello"));       // Freyja says hello
+console.log(dog1.name); // Freyja
+console.log(dog1.speak("hello")); // Freyja says hello
 
 let dog2 = Dog.makeJet();
-console.log(dog2.name);                 // Jet
-
+console.log(dog2.name); // Jet
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
 try {
-	module.exports = Dog;
+  module.exports = Dog;
 } catch {
-	module.exports = null;
+  module.exports = null;
 }
