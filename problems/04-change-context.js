89055f692c9ba3b8af6cb14b2b47b76e4502de5d
seanjeyam <rohit.seanj@gmail.com>
2024-04-30 22:20:00 -0500

    Phase 4

--- problems/04-change-context.js
+++ problems/04-change-context.js
@@ -1,6 +1,27 @@
 function changeContext(func, obj) {
-  // Your code here 
+  // Your code here
+  // const person1 = Person.extractName();
+  // call method invokes the function being indirectly called
+  // bind method returns the function rather than the functions return value
+  /* apply method takes function ex:(Math.max.apply()) and applies that function/method
+   to the given parameters
+   // First parameter refers to scope
+  */
+  return func.call(obj);
 }
+
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+function extractName() {
+  return this.name;
+}
+
+const kristen = new Person("Kristen");
+console.log(changeContext(extractName, kristen)); // => Kristen
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
